8f1158e8b7c543c865fd0ba1612a32c673a1fa63
nottirb <brittonkfischer@gmail.com>
2024-03-30 16:28:59 -0700

    fix: remove debug prints

--- src/games.js
+++ src/games.js
@@ -6,11 +6,6 @@ async function fetchLivePlayerCount() {
     const response = await fetch(`https://games.roproxy.com/v1/games?universeIds=${GAME_UNIVERSE_ID}`);
     const data = await response.json();
 
-    console.log(data);
-    console.log(data.data);
-    console.log(data.data[0]);
-    console.log(data.data[0].playing);
-
     const playerCount = data.data[0].playing;
     document.getElementById(LIVE_PLAYER_COUNT_ELEMENT_ID).textContent = `${playerCount} Playing Now`;
   } catch (error) {
